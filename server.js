e4d37201b604cd0724915452dfb53b97724b322d
hetal <hetal93hasmukh@gmail.com>
2019-07-08 15:16:27 +0530

    third change

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use((req,res,next)=>{
 // app.use((req,res,next)=>{
 //     res.render("maintanance.hbs");
 // });
-app.use(express.static(__dirname+'/public'));
+//app.use(express.static(__dirname+'/public'));
 app.get('/',(req,res)=>{
     res.render(
         "home.hbs",{
